1ed5c951dbabe52b93b45f67f5f481a920b71a5a
christhopermarcelino <christhopermarcelino@gmail.com>
2024-09-12 22:37:33 +0700

    feat: add pagination

---
+++ src/components/Pagination/index.jsx
@@ -0,0 +1,99 @@
+import "./index.css";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Pagination({ movies, page }) {
+  const [maxPage, setMaxPage] = useState(1);
+
+  const getMaxPage = () => {
+    setMaxPage(Math.ceil((movies?.totalResults ?? 0) / 10));
+  };
+
+  useEffect(() => {
+    getMaxPage();
+  }, []);
+
+  const getLeftValue = () => {
+    return page >= maxPage ? maxPage - 2 : page <= 1 ? 1 : Number(page) - 1;
+  };
+
+  const getMiddleValue = () => {
+    return page > 1 && page < maxPage ? page : page <= 1 ? 2 : maxPage - 1;
+  };
+
+  const getRightValue = () => {
+    return page <= 1 ? 3 : page >= maxPage ? maxPage : Number(page) + 1;
+  };
+
+  const getPreviousValue = () => {
+    return page > 1 ? Number(page) - 1 : 1;
+  };
+
+  const getNextValue = () => {
+    return page < maxPage ? Number(page) + 1 : maxPage;
+  };
+
+  const getCurrentShowRange = () => {
+    const from = (page - 1) * 10 + 1;
+    const to = (page - 1) * 10 + movies.Search.length;
+    return `${from} - ${to}`;
+  };
+
+  return (
+    <div className="d-flex justify-content-between align-items-center">
+      <p>
+        Showing {getCurrentShowRange()} of {movies?.totalResults ?? 0} movies
+      </p>
+      <nav>
+        <ul className="pagination">
+          {/* PREVIOUS */}
+          {/* aktif, jika page > 1, else disabled */}
+          {/* href ke page - 1 jika page > 1, else 1 */}
+          <li className={`page-item ${page <= 1 ? "disabled" : ""}`}>
+            <Link className="page-link" to={`/?page=${getPreviousValue()}`}>
+              Previous
+            </Link>
+          </li>
+
+          {/* LEFT */}
+          {/* aktif jika page == 1 */}
+          {/* bernilai page - 1 jika page > 1, else 1 */}
+          {/* jika page >= maxPage, bernilai maxPage - 2, else if page <= 1, bernilai 1, else page - 1 */}
+          <li className={`page-item ${page == 1 ? "active" : ""}`}>
+            <Link className="page-link" to={`/?page=${getLeftValue()}`}>
+              {getLeftValue()}
+            </Link>
+          </li>
+          {/* MIDDLE */}
+          {/* aktif jika page != 1 && page != maxPage */}
+          {/* jika page > 1  && page < maxPage, bernilai page, else if page <= 1, bernilai 2, else maxPage */}
+          <li
+            className={`page-item ${
+              page != 1 && page != maxPage ? "active" : ""
+            }`}
+          >
+            <Link className="page-link" to={`/?page=${getMiddleValue()}`}>
+              {getMiddleValue()}
+            </Link>
+          </li>
+          {/* RIGHT */}
+          {/* aktif jika page == maxPage */}
+          {/* jika page <= 1, bernilai 3, else if page >= maxPage, bernilai maxPage, else page + 1 */}
+          <li className={`page-item ${page == maxPage ? "active" : ""}`}>
+            <Link className="page-link" to={`/?page=${getRightValue()}`}>
+              {getRightValue()}
+            </Link>
+          </li>
+          {/* NEXT */}
+          {/* aktif, jika page < maxPage, else disabled */}
+          {/* href ke page + 1 jika page < maxPage, else maxPage */}
+          <li className={`page-item ${page >= maxPage ? "disabled" : ""}`}>
+            <Link className="page-link" to={`/?page=${getNextValue()}`}>
+              Next
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,24 +3,26 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { changeLoading, changeMovies } from "../store/action";
 import Loading from "../components/Loading";
+import { useSearchParams } from "react-router-dom";
+import Pagination from "../components/Pagination";
 
 const DEFAULT_MOVIE_NAME = "harry%20potter";
 
 export default function Home() {
-  const { movies, isLoading } = useSelector((state) => state);
+  const { movies, isLoading, search } = useSelector((state) => state);
   const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
+  const page = searchParams.get("page") ?? 1;
 
   useEffect(() => {
-    if (!movies?.Search) {
-      getMovieData({ name: DEFAULT_MOVIE_NAME });
-    }
-  }, []);
+    getMovieData({ name: search || DEFAULT_MOVIE_NAME, page });
+  }, [page, search]);
 
-  const getMovieData = async ({ name, page, year }) => {
+  const getMovieData = async ({ name, page }) => {
     try {
       dispatch(changeLoading(true));
       const data = await fetch(
-        `${process.env.REACT_APP_BASE_URL}?apiKey=${process.env.REACT_APP_API_KEY}&s=${name}&page=1`
+        `${process.env.REACT_APP_BASE_URL}?apiKey=${process.env.REACT_APP_API_KEY}&s=${name}&page=${page}`
       );
       const jsonData = await data.json();
       dispatch(changeMovies(jsonData));
@@ -30,23 +32,42 @@ export default function Home() {
     }
   };
 
+  if (isLoading) {
+    return <Loading isFullScreen={false} />;
+  }
+
   return (
     <>
-      {isLoading ? (
-        <Loading isFullScreen={false} />
+      {movies?.Response === "True" ? (
+        <>
+          <h1 className="mb-4 fw-semibold fs-3">
+            {search ? `Search for: ${search}` : "Trending today"}
+          </h1>
+          <div className="row row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-gap-3 mb-4">
+            {(movies?.Search ?? []).map((d) => (
+              <div key={d.imdbID}>
+                <MovieCard
+                  poster={d.Poster}
+                  title={d.Title}
+                  year={d.Year}
+                  href={`/movie/${d.imdbID}`}
+                />
+              </div>
+            ))}
+          </div>
+          <Pagination movies={movies} page={page} />
+        </>
       ) : (
-        <div className="row row-gap-3">
-          <h1 className="mb-4 fw-semibold">Trending today</h1>
-          {(movies?.Search ?? []).map((d) => (
-            <div key={d.imdbID} className="col-6 col-sm-4 col-md-3 col-xl-2">
-              <MovieCard
-                poster={d.Poster}
-                title={d.Title}
-                year={d.Year}
-                href={`/movie/${d.imdbID}`}
-              />
-            </div>
-          ))}
+        <div className="row">
+          <div className="col-4 col-sm-4 col-md-4 col-lg-3 m-auto mb-4">
+            <img
+              src="/assets/images/error.png"
+              alt="Error"
+              className="img-fluid"
+            />
+          </div>
+          <p className="text-center fs-5 mb-0">{movies.Error}</p>
+          <p className="text-center">Please try again your search</p>
         </div>
       )}
     </>
