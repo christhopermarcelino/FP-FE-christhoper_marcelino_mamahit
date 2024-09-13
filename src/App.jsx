8e480a7ec5bc63c138c64a643d0e62a2225313f5
christhopermarcelino <christhopermarcelino@gmail.com>
2024-09-13 13:50:08 +0700

    style: change provider to app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import store from "src/store";
 
 import Home from "src/pages/Home";
 import Movie from "src/pages/Movie";
@@ -9,19 +12,21 @@ import Footer from "src/components/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App bg-light">
-        <Navbar />
-        <main className="container py-5 min-vh-100">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/movie/:imdbID" element={<Movie />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App bg-light">
+          <Navbar />
+          <main className="container py-5 min-vh-100">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/movie/:imdbID" element={<Movie />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
